78a675428a39e416351d0cc58ea290cbb26612a6
curvedinf <40668638+curvedinf@users.noreply.github.com>
2021-07-17 22:06:30 -0400

    New design

---
+++ litecoinz.js
@@ -0,0 +1,11 @@
+$(function() {
+$('#nav-bar').html('<a class="btn btn-outline-secondary" id="nav-index" href="index.html">Home</a>\
+<a class="btn btn-outline-secondary" id="nav-wallets" href="wallets.html">Wallets</a>\
+<a class="btn btn-outline-secondary" id="nav-mining" href="mining.html">Mining</a>\
+<a class="btn btn-outline-secondary" id="nav-community" href="community.html">Community</a>\
+<a class="btn btn-outline-secondary" id="nav-merchants" href="merchants.html">Merchant Integration</a>\
+<a class="btn btn-outline-secondary" id="nav-links" href="links.html">Network Links</a>');
+$('#footer').html('<p class="text-sm fs-6 mt-3 text-black-50">\
+<small><a href="https://litecoinz.org">LitecoinZ.org</a> &mdash; Zk-SNARKs Privacy Coin &mdash; LitecoinZ is the private bank you own. &mdash; \
+<a href="privacy.html">Privacy Policy</a> &mdash; &copy; LitecoinZ Developers</small></p>');
+});
